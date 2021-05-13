e02a804746ac9b2b7dfa6ead7ef2efc6bbd5e704
takahiro.sawakami <takahirosawakami81@gmail.com>
2021-05-13 22:31:01 +0900

    商品一覧ページの作成

--- laravel/app/Http/Controllers/ProductsController.php
+++ laravel/app/Http/Controllers/ProductsController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        return view('index');
+        $products = Product::orderBy('created_at', 'desc')->paginate(6);
+
+        return view('index', [
+            'products' => $products   
+        ]);
     }
 }
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>商品一覧</h1>
+    <div class="text-center index-title">
+        <h1>商品一覧</h1>
+    </div>
+    @if (count($products) > 0)
+        <div class="row">
+        @foreach ($products as $product)
+            <div class="col-4">
+                <div class="card">
+                    <img src="/storage/image/{{ $product->img }}" alt="{{ $product->img }}" class="card-top">
+                    <div class="card-body">
+                        <a href="#" class="card-link">{{ $product->name }}</a>
+                        <p class="card-text">{{ $product->price }}円</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        </div>
+    @endif
+    <div>
+        {{ $products->links() }}
+    </div>
 @endsection
